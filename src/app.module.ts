8fa2b8c7fdea1f6c0e6d6e40d87c142e729fe843
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-10-19 22:56:26 -0300

    feat: Add AuthModule and global ConfigModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AssetsModule } from "./assets/assets.module";
 import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [AssetsModule, UsersModule],
+  imports: [
+    AssetsModule,
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
