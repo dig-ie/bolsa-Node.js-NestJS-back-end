12d5bd9adaca23cc84afb9e05a1f05e6bb404ea2
Thiago Felipe <thiago98felipec@gmail.com>
2025-11-05 10:10:34 -0300

    chore: adding wallet test in test folder.

---
+++ test/wallet.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Wallet (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      //aqui desabilitamos o JwtStrategy só para o teste
+      .overrideProvider('JwtStrategy')
+      .useValue({})
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it("/wallet (GET)", async () => {
+    const res = await request(app.getHttpServer())
+      .get("/wallet")
+      .query({ userId: "admin-001" });
+    expect(res.status).toBe(200);
+    expect(res.body).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
